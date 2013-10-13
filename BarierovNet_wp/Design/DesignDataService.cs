acfe500fb08ba2f75f17cf08170c16d22e53bc33
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-10-13 23:01:22 +0400

    Init commit

---
+++ BarierovNet_wp/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BarierovNet_wp.Model;
+
+namespace BarierovNet_wp.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
